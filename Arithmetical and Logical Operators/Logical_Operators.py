6c244fa71c6b5c1672c279e3d08ee1777635783d
Karel Langer <langerkarel76@gmail.com>
2025-02-12 17:29:38 +0100

    editing_existing_worksheets

---
+++ Arithmetical and Logical Operators/Logical_Operators.py
@@ -0,0 +1,29 @@
+#
+# Napiš funkci, která vrátí hodnotu True, pokud je obchod otevřený a False, pokud je obchod zavřený.
+# Obchod otvírá v 8 hodin a zavírá ve 20 hodin. Čas bude vždy udán celým číslem.
+#
+
+def opening_hours(current_hour):
+    return(8<=current_hour<=20)
+
+""" def opening_hours(current_hour):
+    return(8<=current_hour and current_hour<=20) """
+
+""" def opening_hours(current_hour):
+    if 8<=current_hour and current_hour<=20:
+        return True
+    else:
+        return False """
+
+
+def main() -> None:  # testovací funkce
+    assert opening_hours(12) == True, f"{"\x1b[1;37;41m"}Něco se asi nepovedlo, zkontroluj si funkci ještě jednou.{"\x1b[0m"}"
+    assert opening_hours(7) == False, f"{"\x1b[1;37;41m"}Zdá se, že tvůj obchod otvírá moc brzo.{"\x1b[0m"}"
+    assert opening_hours(21) == False, f"{"\x1b[1;37;41m"}Zdá se, že tvůj obchod je otevřený i po zavírací době.{"\x1b[0m"}"
+    assert opening_hours(8) == True, f"{"\x1b[1;37;41m"}Ujisti se, jestli obchod otvírá hned první hodinu.{"\x1b[0m"}"
+    assert opening_hours(20) == True, f"{"\x1b[1;37;41m"}Ujisti se, jestli má obchod otevřeno i poslední hodinu.{"\x1b[0m"}"
+    print(f"{"\x1b[1;37;42m"}Super práce, vše funguje tak, jak má.{"\x1b[0m"}")
+
+
+if __name__ == '__main__':
+    main()
