3bdcaf48123e4fe2618ce1323579e1ef0e7f7b91
Karel Langer <langerkarel76@gmail.com>
2025-02-12 18:29:18 +0100

    commit

---
+++ Arithmetical and Logical Operators/Calculator.py
@@ -0,0 +1,44 @@
+#
+# Napiš čtyři funkce základních matematických operací - sčítání (addition), odčítání (subtraction), násobení (multiplication) a dělení (division).
+# Každá funkce dostane na vstup dvě celá čísla uložená v proměnných "x" a "y". Může však vrátit i číslo s desetinou čárkou.
+# Při řešení funkcí operací u kterých záleží na pořádí použij "x" jako první (menšenec a dělenec).
+# Pokud příklad nelze vypočítat, vrať hodnotu "False".
+#
+
+def addition(x, y):
+    pass
+
+def subtraction(x, y):
+    pass
+
+def multiplication(x, y):
+    pass
+
+def division(x, y):
+    pass
+
+
+
+def main() -> None:  # testovací funkce
+    assert addition(12, 5) == 17, f"{"\x1b[1;37;41m"}Funkce sčítání nefunguje správně.{"\x1b[0m"}"
+    print(f"{"\x1b[1;37;42m"}Funkce sčítání funguje správně.{"\x1b[0m"}")
+
+    if subtraction(12, 5) == -7:
+        assert subtraction(12, 5) == 7, f"{"\x1b[1;37;41m"}Zkontroluj správné pořádí číslic podle zadání.{"\x1b[0m"}"
+    assert subtraction(12, 5) == 7, f"{"\x1b[1;37;41m"}Funkce odčítání nefunguje správně.{"\x1b[0m"}"
+    print(f"{"\x1b[1;37;42m"}Funkce odčítání funguje správně.{"\x1b[0m"}")
+
+    assert multiplication(5, 3) == 15, f"{"\x1b[1;37;41m"}Funkce násobení nefunguje správně.{"\x1b[0m"}"
+    print(f"{"\x1b[1;37;42m"}Funkce násobení funguje správně.{"\x1b[0m"}")
+
+    if subtraction(3, 15) == 5:
+        assert subtraction(15, 3) == 5, f"{"\x1b[1;37;41m"}Zkontroluj správné pořádí číslic podle zadání.{"\x1b[0m"}"
+    assert division(15, 3) == 5, f"{"\x1b[1;37;41m"}Funkce dělení nefunguje správně.{"\x1b[0m"}"
+    assert division(15, 0) == False, f"{"\x1b[1;37;41m"}Funkce dělení nefunguje správně.{"\x1b[0m"}"
+    print(f"{"\x1b[1;37;42m"}Funkce dělení funguje správně.{"\x1b[0m"}")
+
+    print(f"{"\x1b[1;37;42m"}Super práce, vše funguje tak, jak má.{"\x1b[0m"}")
+
+
+if __name__ == '__main__':
+    main()
